1e46e96f5ba22d70e51ec82769ed432e5923dfed
Gladden <gladdenbackup@gmail.com>
2024-10-12 19:46:17 +0530

    Added Bubble Sort

---
+++ Sorting/Basics.js
@@ -0,0 +1,4 @@
+let arr = [5,1,2,3,4];
+arr.sort();
+
+console.log(arr);
---
+++ Sorting/BubbleSort.js
@@ -0,0 +1,15 @@
+let arr = [6,1,2,7,4,8];
+n = arr.length 
+
+//Sorting in Ascending Order
+
+for(let i=1 ; i<n ; i++){ //passes
+    for(let j=0 ; j<n-i ; j++){ //iterate array
+        if(arr[j]>arr[j+1]){ //comparison
+            temp = arr[j]
+            arr[j] = arr[j+1] 
+            arr[j+1] = temp 
+        }
+    }
+}
+console.log(arr);
